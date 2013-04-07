28bc76c6eabb7062a7d0e127cbbbe9ad1165b589
David Neumann <david_neumann@gmx.net>
2013-04-08 01:46:00 +0200

    ..

---
+++ pong/factory.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/dane-unltd/engine/core"
+	"github.com/dane-unltd/engine/helpers"
+	"github.com/dane-unltd/linalg/matrix"
+)
+
+func createPlayer(id core.EntId, playerNo helpers.Uint32, mut core.MutFuncs) {
+	mut.Mutate("players", id, playerNo)
+	mut.Mutate("score", id, helpers.Uint32(3))
+
+	pos := matrix.VecD{(float64(playerNo) - 1.5) * 100, 0, 0}
+	size := matrix.VecD{5, 20, 5}
+	ori := matrix.VecD{1, 0, 0}
+	mut.Mutate("pos", id, pos)
+	mut.Mutate("vel", id, matrix.ZeroVec(3))
+	mut.Mutate("size", id, size)
+	mut.Mutate("ori", id, ori)
+	mut.Mutate("massinv", id, helpers.Float64(0))
+}
+
+func createBall(mut core.MutFuncs) {
+	id := mut.NewId()
+	mut.Mutate("ball", id, core.Empty{})
+
+	vel := matrix.VecD{-3, 0, 0}
+	size := matrix.VecD{5, 5, 5}
+	ori := matrix.VecD{1, 0, 0}
+	mut.Mutate("pos", id, matrix.ZeroVec(3))
+	mut.Mutate("vel", id, vel)
+	mut.Mutate("size", id, size)
+	mut.Mutate("ori", id, ori)
+	mut.Mutate("massinv", id, helpers.Float64(1))
+}
