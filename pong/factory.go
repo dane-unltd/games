328d83504a0b791a4751e515a7842b52134ef63e
David Neumann <david_neumann@gmx.net>
2013-04-12 01:08:11 +0200

    ..

--- pong/factory.go
+++ pong/factory.go
@@ -10,11 +10,11 @@ func createPlayer(id core.EntId, playerNo helpers.Uint32, mut core.MutFuncs) {
 	mut.Mutate("players", id, playerNo)
 	mut.Mutate("score", id, helpers.Uint32(3))
 
-	pos := matrix.VecD{(float64(playerNo) - 1.5) * 100, 0, 0}
+	pos := matrix.VecD{(float64(playerNo) - 1.5) * 150, 0, 0}
 	size := matrix.VecD{5, 20, 5}
 	ori := matrix.VecD{1, 0, 0}
 	mut.Mutate("pos", id, pos)
-	mut.Mutate("vel", id, matrix.ZeroVec(3))
+	mut.Mutate("vel", id, matrix.NewVecD(3))
 	mut.Mutate("size", id, size)
 	mut.Mutate("ori", id, ori)
 	mut.Mutate("massinv", id, helpers.Float64(0))
@@ -27,7 +27,7 @@ func createBall(mut core.MutFuncs) {
 	vel := matrix.VecD{-3, 0, 0}
 	size := matrix.VecD{5, 5, 5}
 	ori := matrix.VecD{1, 0, 0}
-	mut.Mutate("pos", id, matrix.ZeroVec(3))
+	mut.Mutate("pos", id, matrix.NewVecD(3))
 	mut.Mutate("vel", id, vel)
 	mut.Mutate("size", id, size)
 	mut.Mutate("ori", id, ori)
